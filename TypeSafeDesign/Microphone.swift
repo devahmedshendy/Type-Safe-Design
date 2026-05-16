37706cd7382a87e7cd7f8fb273ac51934fba1bd3
Vlad Grigore Sima <vladgrigoresima10@gmail.com>
2026-05-16 21:11:06 +0300

    Start extracting the condition like: muted, volumes change into types

--- TypeSafeDesign/Microphone.swift
+++ TypeSafeDesign/Microphone.swift
@@ -1,13 +1,30 @@
-enum MicrophoneCapability {
+enum MicrophoneCapabilities {
     case mute
     case volumeChange
 }
 
 struct Microphone {
-    let id: Int
-    let capabilities: [MicrophoneCapability]
+    typealias ID = Int
+    
+    let id: ID
+    let capabilities: [MicrophoneCapabilities]
     let isMuted: Bool
     let volume: Double
+    // step for the volume
     let minVolume: Double
     let maxVolume: Double
+}
+
+protocol MicrophoneCapability {}
+
+struct MuteCapability: MicrophoneCapability {
+    let call: () -> MicrophoneCapability
+}
+
+struct UnmuteCapability: MicrophoneCapability {
+    let call: () -> MicrophoneCapability
+}
+
+struct VolumeUpCapability: MicrophoneCapability {
+    let call: (step: Int) -> Void
 }
--- TypeSafeDesign/MicrophoneServiceFake.swift
+++ TypeSafeDesign/MicrophoneServiceFake.swift
@@ -1,7 +1,7 @@
 final class MicrophoneServiceFake: MicrophoneService {
     
     private struct Defaults {
-        let capabilities: [MicrophoneCapability]
+        let capabilities: [MicrophoneCapabilities]
         let minVolume: Double
         let maxVolume: Double
         let initialVolume: Double
@@ -9,9 +9,9 @@ final class MicrophoneServiceFake: MicrophoneService {
     
     private let defaults: [Int: Defaults] = [
         1: .init(capabilities: [.mute, .volumeChange], minVolume: 0,  maxVolume: 100, initialVolume: 50),
-        2: .init(capabilities: [.mute],                  minVolume: 0,  maxVolume: 80,  initialVolume: 40),
-        3: .init(capabilities: [.volumeChange],         minVolume: 10, maxVolume: 90,  initialVolume: 60),
-        4: .init(capabilities: [],                        minVolume: 0,  maxVolume: 100, initialVolume: 50)
+        2: .init(capabilities: [.mute], minVolume: 0,  maxVolume: 80,  initialVolume: 40),
+        3: .init(capabilities: [.volumeChange], minVolume: 10, maxVolume: 90,  initialVolume: 60),
+        4: .init(capabilities: [], minVolume: 0,  maxVolume: 0, initialVolume: 0)
     ]
     
     private var mutedIDs: Set<Int> = []
@@ -34,7 +34,7 @@ final class MicrophoneServiceFake: MicrophoneService {
     }
     
     func muteMicrophone(id: Int) async throws {
-        try await Task.sleep(for: .milliseconds(50))
+        try await Task.sleep(for: .seconds(1))
         if mutedIDs.contains(id) {
             mutedIDs.remove(id)
         } else {
@@ -43,7 +43,7 @@ final class MicrophoneServiceFake: MicrophoneService {
     }
     
     func setVolume(id: Int, value: Double) async throws {
-        try await Task.sleep(for: .milliseconds(50))
+        try await Task.sleep(for: .seconds(1))
         volumes[id] = value
     }
 }
