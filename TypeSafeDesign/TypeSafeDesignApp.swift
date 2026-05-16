ddb00250f5f42dcda5ff1840e99dcaade5ef6115
Vlad Grigore Sima <vladgrigoresima10@gmail.com>
2026-05-16 10:23:49 +0300

    Initial Commit

---
+++ TypeSafeDesign/TypeSafeDesignApp.swift
@@ -0,0 +1,17 @@
+//
+//  TypeSafeDesignApp.swift
+//  TypeSafeDesign
+//
+//  Created by Vlad Grigore Sima on 16/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct TypeSafeDesignApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
